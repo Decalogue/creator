a5b056ef2185ca9173cb9d2a40d01eab26d05fc6
Decalogue <decalogue80@gmail.com>
2025-12-22 11:38:01 +0800

    update

--- server_vlm.sh
+++ server_vlm.sh
@@ -2,12 +2,15 @@
 ***REMOVED*** ps aux | grep -i vllm | grep -v grep
 
 CUDA_VISIBLE_DEVICES=1 \
+MAX_PIXELS=1003520 \
+VIDEO_MAX_PIXELS=50176 \
+FPS_MAX_FRAMES=8 \
 swift deploy \
-    --model /root/data/AI/pretrain/Qwen2.5-7B-Instruct \
+    --model /root/data/AI/pretrain/Qwen2.5-VL-7B-Instruct \
     --infer_backend vllm \
     --host 0.0.0.0 \
     --port 6002 \
     --vllm_gpu_memory_utilization 0.9 \
     --vllm_max_model_len 8192 \
     --max_new_tokens 2048 \
-    --served_model_name qwen-2.5
+    --served_model_name qwen-2.5-vl
